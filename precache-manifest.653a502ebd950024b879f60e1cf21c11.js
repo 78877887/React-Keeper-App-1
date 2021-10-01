e52e3ff7ddffb437aaf032add06435cc77016308
Paritosh Sharma <paritoshsharma@Paritoshs-Air.bbrouter>
2021-10-01 16:00:27 +0530

    Updates

---
+++ precache-manifest.653a502ebd950024b879f60e1cf21c11.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557080248447246f9457e3e1b2cddebf",
+    "url": "/React-Keeper-App-1/index.html"
+  },
+  {
+    "revision": "022c6ea838fe2ed3dab4",
+    "url": "/React-Keeper-App-1/static/js/2.196ebe37.chunk.js"
+  },
+  {
+    "revision": "b927ad4a763c86aa924c",
+    "url": "/React-Keeper-App-1/static/js/main.ec4a3640.chunk.js"
+  },
+  {
+    "revision": "11b5f0120b0dff67d82d",
+    "url": "/React-Keeper-App-1/static/js/runtime-main.01dd1a46.js"
+  }
+]);
